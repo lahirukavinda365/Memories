a67bb1cb63b714ff56e6d83471cb36c377dd1714
Lahiru Kavinda <lahirukavinda365@gmail.com>
2022-08-08 02:51:40 +0700

    Folder Structure created, Basic setup done, Development started

---
+++ client/src/component/Posts/Post/Post.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import useStyles from './Style';
+
+
+const Post = () => {
+ const classes = useStyles();
+
+  return (
+    <div>
+       <h1>Post</h1>
+    </div>
+  )
+}
+
+export default Post
